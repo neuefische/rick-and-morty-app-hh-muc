f1245c44641a4d97a84cfd19dc15b7b334ca6bc9
marwinburesch <marwin@posteo.de>
2020-11-03 15:32:54 +0100

    Implement basic Routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import { useEffect, useState } from "react";
 import Character from "./Character";
 import getCharacters from "./services/getCharacters";
+import { Switch, Route, Link } from "react-router-dom";
 
 function App() {
-  const [characters, setCharacters] = useState([])
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
     getCharacters()
-    .then(data => setCharacters(data.results))
-    .catch(error => console.log(error))
-  }, [])
+      .then((data) => setCharacters(data.results))
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
     <div className="App">
-      {characters.map(({id, name, image}) =>
-      <Character key={id} name={name} imgUrl={image} />)}
+      <Switch>
+        <Route path="/characters">
+          <Link to="/">Home</Link>
+          {characters.map(({ id, name, image }) => (
+            <Character key={id} name={name} imgUrl={image} />
+          ))}
+        </Route>
+        <Route path="/">
+          <h1>Welcome</h1>
+          <Link to="/characters">Characters</Link>
+        </Route>
+      </Switch>
     </div>
   );
 }
